48b2a7bf4c8f47b7d13bb1ac3a4ce663afc07320
韩昕睿 <22371298@buaa.edu.cn>
2024-11-24 14:11:24 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+import '@styles/common.scss'
 
 const app = createApp(App)
 
--- vite.config.js
+++ vite.config.js
@@ -9,10 +9,27 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    Components({
+      resolvers: [
+        // 1. 配置elementPlus采用sass样式配色系统
+        ElementPlusResolver({ importStyle: "sass" }),
+      ],
+    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 自动导入scss文件
+        additionalData: `
+          @use "@/styles/element/index.scss" as *;
+          @use "@/styles/var.scss" as *;
+        `,
+      }
+    }
+  }
 })
